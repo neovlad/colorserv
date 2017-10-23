b6db77c068fcbeeb808e1ef827d03118a6e3f49a
neovlad <neovlad@gmail.com>
2017-10-23 11:32:08 -0700

    little cleanup

--- source/server.py
+++ source/server.py
@@ -7,16 +7,13 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def upload_file():
-    #if request.args.get()
-
-    # check if the post request has the file part
-
     if len(request.args) > 0:
         outfile = "./static/video/temp.mp4"
         poc = "./static/processed/processed.mp4"
 
         if os.path.isfile(outfile):
             os.remove(outfile)
+
         if os.path.isfile(outfile):
             os.remove(outfile)
 
